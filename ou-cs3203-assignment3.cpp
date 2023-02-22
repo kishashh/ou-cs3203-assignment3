4542ca69fc51e9e78ff95dab974b3f8ffb24b63e
kishashh <48453359+kishashh@users.noreply.github.com>
2023-02-21 20:06:41 -0600

    Organization
    commit branch for organization

--- ou-cs3203-assignment3.cpp
+++ ou-cs3203-assignment3.cpp
@@ -4,52 +4,60 @@ using namespace std;
 
 int listSum(int* ptr, int size){ //loop to add each element to sum
     int sum = 0;
-    for(int i = 0; i < size; i++){
+
+    for(int i = 0; i < size; i++){//callculates sum
         sum = sum + ptr[i];
     }
+    
     return sum;
 }
 
 int listProduct(int* ptr, int size){//loop to multiply each element to product
     int product = 1;
-    for(int i = 0; i < size; i++){
+
+    for(int i = 0; i < size; i++){//calculates product
         product = product * ptr[i];
     }
+
     return product;
 }
 
 int* reverseList(int* list, int size){
     int* listReverse = new int[size];
-    for(int i = 0; i < size; i++){
+
+    for(int i = 0; i < size; i++){//creates reverse loop
         listReverse[i] = list[size - 1 - i]; //size - 1 - (amount of loop iterations)
     }
+
     return listReverse;
 }
 
 int main(){
     int n;
-    cout << "Enter the how many numbers will be in the list: ";
+    cout << "Enter the array size: ";
     cin >> n;
 
     int* numArray = new int[n]; //an array with a size that is determined at runtime
 
     for (int i = 0; i < n; i++){
-        cout << "Enter the value at " << i << ": ";
+        cout << "Enter the value of " << i << ": ";
         cin >> numArray[i];
     }
 
     int sum = listSum(numArray, n);
-    cout << "The sum of the array is: " << sum << endl;
+    cout << "The sum of all values in the array is: " << sum << endl;
     
     int product = listProduct(numArray, n);
-    cout << "The product of the array is: " << product << endl;
+    cout << "The product of all values in the array is: " << product << endl;
 
     int* numReverse;
     numReverse = reverseList(numArray, n);
     cout << "The reverse of the array:";
-    for (int i = 0; i < n; i++) {
+
+    for (int i = 0; i < n; i++) {//prints reversed array
         cout << " " << numReverse[i];
     }
+
     cout << endl;
 
     delete[] numArray;//free memory
