dd6cfa58c462b8fafe01af3100e01c85e2283174
kishashh <48453359+kishashh@users.noreply.github.com>
2023-02-21 18:59:38 -0600

    Adds listProduct()
    listProduct() multiplies each element in the array and prints the product

--- ou-cs3203-assignment3.cpp
+++ ou-cs3203-assignment3.cpp
@@ -10,6 +10,14 @@ int listSum(int* ptr, int size){ //loop to add each element to sum
     return sum;
 }
 
+int listProduct(int* ptr, int size){//loop to multiply each element to product
+    int product = 1;
+    for(int i = 0; i < size; i++){
+        product = product * ptr[i];
+    }
+    return product;
+}
+
 int main(){
 
     int numArray[3];
@@ -20,6 +28,9 @@ int main(){
 
     int sum = listSum(numArray, sizeof(numArray)/sizeof(int));
     cout << "The sum of the array is: " << sum << endl;
+    
+    int product = listProduct(numArray, sizeof(numArray)/sizeof(int));
+    cout << "The product of the array is: " << product << endl;
 
     return 0;
 }
